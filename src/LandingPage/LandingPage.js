c19c141452871f52d41e5ab68468edb8b8dbd1cf
RashBM <57621825+RashBM@users.noreply.github.com>
2021-10-29 20:47:59 +0100

    add searchSuggestions

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -2,6 +2,7 @@ import TopNav from "./TopNav/TopNav";
 import logo from "../assets/logo.png";
 import styles from "./LandingPage.module.css";
 import SearchBar from "../SearchBar/SearchBar";
+import SearchSuggestions from "./SearchSuggestions/SearchSuggestions";
 
 function LandingPage() {
   return (
@@ -9,6 +10,7 @@ function LandingPage() {
       <TopNav />
       <img src={logo} className={styles.logo} alt="logo" />
       <SearchBar />
+      <SearchSuggestions />
     </div>
   );
 }
