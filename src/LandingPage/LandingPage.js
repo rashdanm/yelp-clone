419f98215b8ea9605693a58a6491fe4c92440596
RashBM <57621825+RashBM@users.noreply.github.com>
2021-10-29 18:52:36 +0100

    logo

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,9 +1,12 @@
 import TopNav from "./TopNav/TopNav";
+import logo from "../assets/logo.png";
+import styles from "./LandingPage.module.css";
 
 function LandingPage() {
   return (
     <div>
       <TopNav />
+      <img src={logo} className={styles.logo} alt="logo" />
     </div>
   );
 }
