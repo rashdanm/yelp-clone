d18ef7140dbda67a398488ede4b4b2475e022bd9
RashBM <57621825+RashBM@users.noreply.github.com>
2021-10-29 21:02:40 +0100

    amend layout

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -6,11 +6,13 @@ import SearchSuggestions from "./SearchSuggestions/SearchSuggestions";
 
 function LandingPage() {
   return (
-    <div>
-      <TopNav />
-      <img src={logo} className={styles.logo} alt="logo" />
-      <SearchBar />
-      <SearchSuggestions />
+    <div className={styles["landing"]}>
+      <div className={styles["search-area"]}>
+        <TopNav />
+        <img src={logo} className={styles.logo} alt="logo" />
+        <SearchBar />
+        <SearchSuggestions />
+      </div>
     </div>
   );
 }
