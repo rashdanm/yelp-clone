3b718933151fdef85a28a1e98c34126ed7f1e0d3
RashBM <57621825+RashBM@users.noreply.github.com>
2021-10-30 15:42:18 +0100

    search results summary component

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,12 +1,14 @@
 import NavBar from "../NavBar/NavBar";
 import SubNav from "../SubNav/SubNav";
 import styles from "./Search.module.css";
+import SearchResultsSummary from "./SearchResultsSummary/SearchResultsSummary";
 
 function Search() {
   return (
     <div>
       <NavBar />
       <SubNav />
+      <SearchResultsSummary />
     </div>
   );
 }
